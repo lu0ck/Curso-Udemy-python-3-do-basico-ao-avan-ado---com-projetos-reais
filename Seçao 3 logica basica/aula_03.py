e3229eeb320424be3cd50cba2a8eadb2ca9d5461
Lokia <lucaspaixaoprofissional@gmail.com>
2025-05-10 14:43:51 -0300

    aula 3

---
+++ Seçao 3 logica basica/aula_03.py
@@ -0,0 +1,16 @@
+'''
+python = linguagem de programacao
+tipo de tipagem = dinamica
+tipagem = forte
+str = string = texto
+strings sao textos que estao dentro das aspas
+'''
+
+#aspas simples
+print('Hello World')
+#aspas duplas
+print("Hello World")
+#escape
+print('Hello \'World\'')
+#r
+print(r'Hello \'World\'')
