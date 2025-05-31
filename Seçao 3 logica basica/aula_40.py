ff8f5c0abe8617129161fcddccfd92327e0b36ed
Lokia <lucaspaixaoprofissional@gmail.com>
2025-05-31 15:42:43 -0300

    correçao aula 41

--- Seçao 3 logica basica/aula_40.py
+++ Seçao 3 logica basica/aula_40.py
@@ -1,3 +1,4 @@
+"""
 while True:
     numero_1 = input('Digite um número: ')
     operador = input('Digite o operador (+, -, *, /): ')
@@ -17,3 +18,40 @@ while True:
         else:
             resultado = float(numero_1) / float(numero_2)
             print(f'{numero_1} / {numero_2} = {resultado}')
+"""
+#correçao da aula
+""" Calculadora com while """
+while True:
+    numero_1 = input('Digite um número: ')
+    numero_2 = input('Digite outro número: ')
+    operador = input('Digite o operador (+-/*): ')
+
+    numeros_validos = None
+
+    try:
+        num_1_float = float(numero_1)
+        num_2_float = float(numero_2)
+        numeros_validos = True
+    except:
+        numeros_validos = None
+
+    if numeros_validos is None:
+        print('Um ou ambos os números digitados são inválidos.')
+        continue
+
+    operadores_permitidos = '+-/*'
+
+    if operador not in operadores_permitidos:
+        print('Operador inválido.')
+        continue
+
+    if len(operador) > 1:
+        print('Digite apenas um operador.')
+        continue
+
+    ###
+
+    sair = input('Quer sair? [s]im: ').lower().startswith('s')
+
+    if sair is True:
+        break
