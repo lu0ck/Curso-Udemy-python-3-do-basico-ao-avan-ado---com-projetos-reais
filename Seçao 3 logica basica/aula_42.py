300590a9f7c62e9c621e29be3b7b063760d31e3e
Lokia <lucaspaixaoprofissional@gmail.com>
2025-06-01 12:24:09 -0300

    aula 42

---
+++ Seçao 3 logica basica/aula_42.py
@@ -0,0 +1,9 @@
+frase = (
+    'o Python é uma linguagem de programaçao.'
+    'multiparadigma.'
+    'Python foi criado por guido van rossum.'
+    'em 1991.'
+)
+
+print(frase.count('a'))  # Conta quantas vezes 'Python' aparece na string
+
