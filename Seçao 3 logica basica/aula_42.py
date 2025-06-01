97f0552736b5acf6e69f9ffb0cbab776e7b16964
Lokia <lucaspaixaoprofissional@gmail.com>
2025-06-01 12:30:23 -0300

    aula 42

--- Seçao 3 logica basica/aula_42.py
+++ Seçao 3 logica basica/aula_42.py
@@ -1,3 +1,4 @@
+"""
 frase = (
     'o Python é uma linguagem de programaçao.'
     'multiparadigma.'
@@ -7,3 +8,30 @@ frase = (
 
 print(frase.count('a'))  # Conta quantas vezes 'Python' aparece na string
 
+"""
+frase = 'aaaooo'
+
+i = 0
+qtd_apareceu_mais_vezes = 0
+letra_apareceu_mais_vezes = ''
+
+while i < len(frase):
+    letra_atual = frase[i]
+
+    if letra_atual == ' ':
+        i += 1
+        continue
+
+    qtd_apareceu_mais_vezes_atual = frase.count(letra_atual)
+
+    if qtd_apareceu_mais_vezes < qtd_apareceu_mais_vezes_atual:
+        qtd_apareceu_mais_vezes = qtd_apareceu_mais_vezes_atual
+        letra_apareceu_mais_vezes = letra_atual
+
+    i += 1
+
+print(
+    'A letra que apareceu mais vezes foi '
+    f'"{letra_apareceu_mais_vezes}" que apareceu '
+    f'{qtd_apareceu_mais_vezes}x'
+)
